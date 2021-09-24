d9b630e27387e7ac19d4a153ec5c62c3290c9c1c
Malvi17 <85649880+Malvi17@users.noreply.github.com>
2021-09-24 18:51:39 +0200

    Add files via upload

---
+++ src/gestore.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%%% @author malvi
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 06. Jun 2021 19:14
+%%%-------------------------------------------------------------------
+-module(gestore).
+-author("malvi").
+
+%% API
+-export([start_link/0]).
+
+%%Funzione che genera il server e gli aggiunge l'handler, infine ritorna il Pid.
+start_link() ->
+  {ok, Pid} = gen_event:start_link(),
+  gen_event:add_handler(Pid, eventHandler, []),
+
+  {ok,Pid}.

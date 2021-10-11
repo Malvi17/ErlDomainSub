209212d800c847c55ea99335c8cbb3f0f54dbe16
Malvi17 <85649880+Malvi17@users.noreply.github.com>
2021-10-11 14:02:19 +0200

    Update eventHandler.erl

--- Src/eventHandler.erl
+++ Src/eventHandler.erl
@@ -13,10 +13,8 @@
   -export([init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3,
     terminate/2, crea/1, get_timestamp/0, printLog/4, printLogFree/4]).
 
-  %%Utilizza la funzione crea
+  
   init([]) ->
-
-    %%crea(1),
     {ok, []}.
 
 
@@ -42,12 +40,10 @@
     {ok, State};
 
   handle_event({pendingDelete, FirstTime, Time3, Domain}, State) ->
-    %%io:format("Arrivato~n"),
     spawn(eventHandler,printLog,[FirstTime,Time3,Domain,pendingDelete]),
     {ok, State};
 
   handle_event({limitExceeded, FirstTime, Time3, Domain}, State) ->
-    %%io:format("Arrivato~n"),
     spawn(eventHandler,printLog,[FirstTime,Time3,Domain,limitExceeded]),
     exit({limitExceeded, State});
 
@@ -93,22 +89,6 @@
   printLog(FirstTime, Time3, Domain,redemptionPeriod) ->
     TimeStamp = calendar:system_time_to_rfc3339(os:system_time(millisecond), [{unit, millisecond}]),
     file:write_file("txt/"++Domain++".txt",[io_lib:format("[~p] status=redemptionPeriod StartedAt: ~p Elapse: ~p ms~n",[TimeStamp,FirstTime,Time3])], [append]).
-
-
-
-
-  %%Funzione che crea 4 processi, generati come handler di erlbus attraverso spawn_handler.
-  %%Attraverso sub invece si sottoscrive al topic domain
-  crea(N) ->
-    case N of
-      0 -> io:format("Tutti i processi sono stati creati~n");
-      _->
-        H = ebus_proc:spawn_handler(fun provaSub:handle_msg/2,[self()]),
-        ebus:sub(H,domain),
-        io:format("Processo ~p creato e subbato al topic domain~n",[H]),
-        crea(N-1)
-
-    end.
 
   get_timestamp() ->
     {Mega, Sec, Micro} = os:timestamp(),
